47795e11b2be2243bfb8d6304271c341da4c29b5
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2024-11-25 13:37:21 +0530

    have checked wroking of user routing

--- PizzaAppBackend/src/controllers/userController.js
+++ PizzaAppBackend/src/controllers/userController.js
@@ -1,5 +1,14 @@
-function createUser(){
-    console.log("controller called");
+function createUser(req, res){
+    console.log("Create user: controller called");
+    console.log(req.body);
+    // TODO -> Register the user
+
+    return res.json({
+        message: "OK"
+    });
 }
 
-module.exports = createUser;
+// module.exports = createUser;  it is wrong becuase we have to return the function but here it is returning object
+module.exports = { 
+    createUser      // now it is returning function
+};             
