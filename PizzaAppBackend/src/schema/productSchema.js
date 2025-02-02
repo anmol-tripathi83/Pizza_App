5edaf6d49a698f454fb8b7a78d66a3725d9d185e
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-02-02 16:32:13 +0530

    Basic Product Schema done

---
+++ PizzaAppBackend/src/schema/productSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+// This Schema is based on our app perspective
+const productSchema = new mongoose.Schema({
+    productName:{
+        type: String,
+        required: [ true, "Product name is required"],
+        minLength: [5, "Product name must be atleast 5 character long"],
+        trim: true
+    },
+    description :{
+        type: String,
+        minLength: [5, "Product description must be atleast 5 character long"]
+    },
+    productImage: {
+        type: String,
+    },
+    price:{
+        type: Number,
+        required: [true, "Price is required"],
+    },
+    category:{
+        type: String,
+        enum: ['veg', 'non-veg', 'drinks', 'sides'],    // property which tells that category can be either of four choices in the array
+        default: 'veg'    // if category is not provided then 'veg' is default category
+    },
+    inStock: {
+        type: Boolean,
+        required: [true, "In stock status is required"],
+        default: true
+    } 
+} , { timestamps: true });
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
