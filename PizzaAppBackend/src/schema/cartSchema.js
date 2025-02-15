cd648b5aa6935574f26d65358e4b6a660cd3f511
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-02-16 01:41:27 +0530

    Added cart schema

---
+++ PizzaAppBackend/src/schema/cartSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Cart schema
+const cartSchema = new mongoose.Schema({
+    user: {   // association to other schema(it require only the objectid of user schema(ref will govern this))
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true,
+        ref: 'User'    
+    },
+    items: [    // hum on the spot cart totalprice nikalenge with the use of productid and its quantity
+        {          // items ek array hoga
+            product: {  // product ek object hoga
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'Product'
+            },
+            quantity: {  // quantity ek number hoga
+                type: Number,
+                required: true,
+                default: 1
+            }
+        }
+    ],
+},{timestamps:true});
+
+const cart = mongoose.model('cart',cartSchema);
+
+module.exports = cart;
