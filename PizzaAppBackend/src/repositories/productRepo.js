cf96297a15293d4474ca9c8506c10519ea9fd258
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-02-18 01:52:16 +0530

    Added functionality of deleting cloudinary(productImage) while deleting the product

--- PizzaAppBackend/src/repositories/productRepo.js
+++ PizzaAppBackend/src/repositories/productRepo.js
@@ -1,6 +1,9 @@
+const mongoose = require("mongoose");
+const cloudinary  = require('../config/cloudinaryConfig');
 const Product = require('../schema/productSchema');
 const BadRequestError = require('../utils/badRequest');
 const InternalServerError = require('../utils/InternalServerError');
+const NotFoundError = require('../utils/notFoundError');
 
 // interacting with the database(creating a new product with following product details which is in productDetails object)
 async function createProduct(ProductDetails){
@@ -31,14 +34,44 @@ async function getProductById(productId){
     }
 }
 
+//fetching public id(for deleting) from image URL(cloudinary URL)
+const getPublicIdFromUrl = (url) => {
+    const regex = /\/upload\/(?:v\d+\/)?(.+)\./;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+};
+
 // Function to interact with DB and deleting product by their id
 async function deleteProductById(productId){
+    const session = await mongoose.startSession();   // start mongodb transaction
+    session.startTransaction();
     try{
-        const response = await Product.findByIdAndDelete(productId);
+        // 1. Fetch the product from the database
+        const product = await Product.findById(productId);
+        if(!product){
+            await session.abortTransaction(); // Abort transaction
+            session.endSession();
+            throw new NotFoundError("Product");
+        }
+        // 2. Attempt to Delete the product from MongoDB
+        const response = await Product.findByIdAndDelete(productId).session(session);
+
+        // 3. if successful, then Delete the image from Cloudinary
+        if(product.productImage){
+            const publicId = getPublicIdFromUrl(product.productImage);  // getting public id from cloudinary
+            if(publicId){
+                const cloudinaryResponse = await cloudinary.uploader.destroy(publicId);
+            }
+        }
+        // 4. Commit transaction
+        await session.commitTransaction();
         return response;
     } catch(error){
+        await session.abortTransaction();
         console.log(error);
-        throw new InternalServerError();   // DB se related issue
+        throw new InternalServerError();   // DB se related 
+    } finally {
+        session.endSession(); // End session
     }
 }
 
