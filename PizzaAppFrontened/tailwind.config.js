d09c866b6daf44023372a9e745699cde19065476
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-03-22 15:15:41 +0530

    Tailwind setup with vite done

---
+++ PizzaAppFrontened/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [         // this will ensures that where tailwind enable
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
