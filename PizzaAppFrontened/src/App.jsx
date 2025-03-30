c600d59c0b5fcc7bb5b884267db52645496b3d22
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2025-03-30 18:21:43 +0530

    Added Signup page

--- PizzaAppFrontened/src/App.jsx
+++ PizzaAppFrontened/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './Pages/Home';
+import SignUp from './Pages/Auth/Signup';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={ <Home/> } />
+          <Route path="/auth/signup" element={<SignUp/>} />
         </Routes>
       </div>
   );
